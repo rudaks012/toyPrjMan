2d79bb8959d0d9c772a3cacd9e001e3091c1591a
gyeongman <rudaks012@naver.com>
2023-02-06 22:53:37 +0900

    Feat : DTO의 테스트 코드 주석 꼭확인

--- src/main/java/com/toyprj/toyman/web/HelloController.java
+++ src/main/java/com/toyprj/toyman/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.toyprj.toyman.web;
 
+import com.toyprj.toyman.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +12,10 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/test/java/com/toyprj/toyman/web/HelloControllerTest.java
+++ src/test/java/com/toyprj/toyman/web/HelloControllerTest.java
@@ -1,20 +1,21 @@
 package com.toyprj.toyman.web;
 
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.toyprj.toyman.web.HelloController;
+import java.util.regex.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @ExtendWith(SpringExtension.class)
 // @WebMvcTest: @Controller, @ControllerAdvice 등을 사용할 수 있음
@@ -39,4 +40,23 @@ public class HelloControllerTest {
         // 응답 본문의 내용을 검증 (Controller에서 "Hello, World!"를 리턴하기 때문에 이 값이 맞는지 검증)
     }
 
+
+    @Test
+    @DisplayName("HelloDto가 리턴된다")
+    void helloDtoReturn() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+               .param("name", name) //API 테스트할때 사용될 요청 파라미터 설정 , 단 String만 허용됨 날짜,숫자 -> 문자열 변환 해야함
+               .param("amount", String.valueOf(amount)))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.name", is(name))) //JSON응답 값을 필드별로 검증 -> $를 기준으로 필드명을 명시
+           .andExpect(jsonPath("$.amount", is(amount)));
+
+        // when
+
+        // then
+    }
 }
