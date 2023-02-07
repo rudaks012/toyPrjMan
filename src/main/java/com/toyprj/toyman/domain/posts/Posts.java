b31b17a7fe628cfad1b82453c9b972c1cf13f39b
gyeongman <rudaks012@naver.com>
2023-02-07 20:55:21 +0900

    Chore : 소스 공부용 주석

--- src/main/java/com/toyprj/toyman/domain/posts/Posts.java
+++ src/main/java/com/toyprj/toyman/domain/posts/Posts.java
@@ -10,7 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor // 기본 생성자 자동 추가 (public Posts() {}와 같은 효과)
 @Entity // 테이블과 링크될 클래스임을 나타냄 (기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭)
 public class Posts {
 
@@ -26,7 +26,7 @@ public class Posts {
 
     private String author;
 
-    @Builder
+    @Builder // 해당 클래스의 빌더 패턴 클래스를 생성 (생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함)
     public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
