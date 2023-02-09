1a5dc7fdab0cf09dcba990ce63bcd4242f2d6f82
gyeongman <rudaks012@naver.com>
2023-02-09 22:34:32 +0900

    Feat : JPA H2 사용으로 TestCode 작성

--- src/test/java/com/toyprj/toyman/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/toyprj/toyman/web/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,45 @@
 package com.toyprj.toyman.web.domain.posts;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.toyprj.toyman.domain.posts.Posts;
+import com.toyprj.toyman.domain.posts.PostsRepository;
+import java.util.List;
+import org.junit.After;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class PostsRepositoryTest {
 
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // Junit에서 단위 테스트가 끝날 때마다 수행되는 메소드를 지정 (보통은 배포 전 전체 테스트를 수행할 때 테스트간 데이터 침범을 막기 위해 사용)
+    // 여러 테스트가 동시에 수행되면 테스트용 데이터베이스인 H2에 데이터가 그대로 남아 있어 다음 테스트 실행 시 테스트가 실패할 수 있음
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 불러오기")
+    void postSaveLoad() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // 테이블 posts에 insert/update 쿼리를 실행 id 값이 있다면 update가 없다면 insert 쿼리 실행
+                                  .title(title)
+                                  .content(content)
+                                  .author("jojoldu@gmail.com")
+                                  .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll(); // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 }
