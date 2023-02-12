f47a5d3c484f76b6ccb6855685749190a6d55ed0
gyeongman <rudaks012@naver.com>
2023-02-12 22:17:24 +0900

    Refactor : 공부를 위한 주석 추가

--- src/test/java/com/toyprj/toyman/web/HelloControllerTest.java
+++ src/test/java/com/toyprj/toyman/web/HelloControllerTest.java
@@ -29,10 +29,15 @@ import org.springframework.test.web.servlet.MockMvc;
 public class HelloControllerTest {
 
     @Autowired
+    // @Autowired: 스프링이 관리하는 Bean을 주입받음
+
     private MockMvc mvc; // MockMvc: 웹 API를 테스트할 때 사용
+    // 스프링 MVC 테스트의 시작점
 
     @Test
+    @DisplayName("hello가_리턴된다")
     public void hello가_리턴된다() throws Exception {
+        // @DisplayName: 테스트 메소드의 의도를 설명
         String hello = "Hello, World!";
 
         mvc.perform(
@@ -42,6 +47,7 @@ public class HelloControllerTest {
            .andExpect(
                content().string(hello)); // mvc.perform의 결과를 검증 (MockMvcResultMatchers.content)
         // 응답 본문의 내용을 검증 (Controller에서 "Hello, World!"를 리턴하기 때문에 이 값이 맞는지 검증)
+
     }
 
 
