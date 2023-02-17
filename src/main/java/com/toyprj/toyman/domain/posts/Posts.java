82b64941702558a8c4f60e6823b2e86ed94c298f
gyeongman <rudaks012@naver.com>
2023-02-17 18:19:19 +0900

    Feat : BaseTimeEntity 생성

--- src/main/java/com/toyprj/toyman/domain/posts/Posts.java
+++ src/main/java/com/toyprj/toyman/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.toyprj.toyman.domain.posts;
 
+import com.toyprj.toyman.domain.BaseTimeEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor // 기본 생성자 자동 추가 (public Posts() {}와 같은 효과)
 @Entity // 테이블과 링크될 클래스임을 나타냄 (기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭)
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성 규칙을 나타냄 (기본값은 AUTO, MySQL의 auto_increment와 같이 자동증가하는 정수형 값이 됨)
