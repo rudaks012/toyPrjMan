875b86595f34c7445b6d69130cd173635c359243
gyeongman <rudaks012@naver.com>
2023-02-19 00:20:42 +0900

    Feat : 리액트 연동 -> 다시 수정해봐야함

--- src/main/java/com/toyprj/toyman/web/HelloController.java
+++ src/main/java/com/toyprj/toyman/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.toyprj.toyman.web;
 
 import com.toyprj.toyman.web.dto.HelloResponseDto;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
+    @GetMapping("/hello1")
+    public List<String> hello() {
+        return  Arrays.asList("서버서버", "뷰뷰");
     }
 
     @GetMapping("/hello/dto")
