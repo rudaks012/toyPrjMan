1f2efb41ea5435b14cfa27fb852b3d85dd6c8cbf
gyeongman <rudaks012@naver.com>
2023-02-18 22:38:34 +0900

    Feat : BaseTimeEntity TestCode

--- src/main/java/com/toyprj/toyman/ToymanApplication.java
+++ src/main/java/com/toyprj/toyman/ToymanApplication.java
@@ -3,17 +3,15 @@ package com.toyprj.toyman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class ToymanApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		SpringApplication.run(ToymanApplication.class, args);
-	}
-	@Bean
-	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
-		return new HiddenHttpMethodFilter();
 	}
 }
--- src/test/java/com/toyprj/toyman/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/toyprj/toyman/web/domain/posts/PostsRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.toyprj.toyman.domain.posts.Posts;
 import com.toyprj.toyman.domain.posts.PostsRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.After;
 import org.junit.jupiter.api.DisplayName;
@@ -41,5 +42,26 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @DisplayName("BaseTimeEntity 등록")
+    void BaseTimeEntityRegistration() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                                  .title("title")
+                                  .content("content")
+                                  .author("author")
+                                  .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
