5cd6e6a3347b4edc57fe4fda8a4204d122a9da4e
gyeongman <rudaks012@naver.com>
2023-03-01 18:51:09 +0900

    Feat : 전체 리스트 조회

--- src/main/java/com/toyprj/toyman/service/posts/PostsService.java
+++ src/main/java/com/toyprj/toyman/service/posts/PostsService.java
@@ -2,16 +2,19 @@ package com.toyprj.toyman.service.posts;
 
 import com.toyprj.toyman.domain.posts.Posts;
 import com.toyprj.toyman.domain.posts.PostsRepository;
+import com.toyprj.toyman.web.dto.PostsListResponseDto;
 import com.toyprj.toyman.web.dto.PostsResponseDto;
 import com.toyprj.toyman.web.dto.PostsSaveRequestDto;
 import com.toyprj.toyman.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
 // @Service // 서비스 클래스에는 @Service 어노테이션을 사용
 // @Transactional // 트랜잭션, 서비스 클래스에는 @Transactional 어노테이션을 사용
+@RequiredArgsConstructor// final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
 @Service
 public class PostsService {
 
@@ -39,5 +42,12 @@ public class PostsService {
         // PostsResponseDto dto = new PostsResponseDto(entity);
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto -> new PostsListResponseDto(PostsListResponseDto))
+            .collect(Collectors.toList());
     }
 }
--- src/main/java/com/toyprj/toyman/web/IndexController.java
+++ src/main/java/com/toyprj/toyman/web/IndexController.java
@@ -1,16 +1,20 @@
 package com.toyprj.toyman.web;
 
+import com.toyprj.toyman.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor// final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
-        System.out.println("Hello, World!");
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
@@ -18,5 +22,7 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+
 
 }
