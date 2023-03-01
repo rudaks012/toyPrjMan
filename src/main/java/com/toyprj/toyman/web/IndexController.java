de1e281c9da813bd47cf3fbfb4f05edbfb6644c5
gyeongman <rudaks012@naver.com>
2023-03-01 17:32:00 +0900

    Feat : Index.js 추가

--- src/main/java/com/toyprj/toyman/web/IndexController.java
+++ src/main/java/com/toyprj/toyman/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.toyprj.toyman.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor// final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String index() {
         System.out.println("Hello, World!");
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
 }
