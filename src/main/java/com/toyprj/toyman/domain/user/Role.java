1bb2109f795ab381339ded55cf9fcc5178a51bc7
gyeongman <rudaks012@naver.com>
2023-03-05 16:55:05 +0900

    Feat : 구글 로그인 Enum 설정

---
+++ src/main/java/com/toyprj/toyman/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.toyprj.toyman.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor // 선언된 모든 final 필드가 포함된 생성자를 생성해줌
+public enum Role {
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+
+}
