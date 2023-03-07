8eba01669977414e0eb977e5bd52d44aeb10377e
gyeongman <rudaks012@naver.com>
2023-03-07 22:07:12 +0900

    Feat : 구글 로그인 설정

--- src/main/java/com/toyprj/toyman/web/IndexController.java
+++ src/main/java/com/toyprj/toyman/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.toyprj.toyman.web;
 
+import com.toyprj.toyman.config.auth.dto.SessionUser;
 import com.toyprj.toyman.domain.posts.Posts;
 import com.toyprj.toyman.service.posts.PostsService;
 import com.toyprj.toyman.web.dto.PostsListResponseDto;
 import com.toyprj.toyman.web.dto.PostsResponseDto;
 import java.util.List;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,10 +18,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
