e9d1d1ec11fc1e7b36ddac87fbd6baa83985a3e1
gyeongman <rudaks012@naver.com>
2023-03-11 23:10:23 +0900

    Refacotr : 반복된 부분 Anotation 추가 후 생산성 증가, SessionStore 추가

--- src/main/java/com/toyprj/toyman/domain/posts/Posts.java
+++ src/main/java/com/toyprj/toyman/domain/posts/Posts.java
@@ -22,6 +22,7 @@ public class Posts extends BaseTimeEntity {
     @Column(length = 500, nullable = false) // 테이블의 칼럼을 나타냄 (기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용)
     private String title;
 
+    //columnDefinition을 사용하면 테이블의 칼럼을 선언할 때 타입을 지정할 수 있음
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
